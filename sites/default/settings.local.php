7b81840f5f6a01a8521f24148ac2f1baa4dc6869
Juan Carielo <juancarielo@gmail.com>
2019-10-01 10:55:59 -0300

    Created settings, services and fix gitignore

---
+++ sites/default/settings.local.php
@@ -0,0 +1,23 @@
+<?php
+// Database
+$databases['default']['default'] = array (
+  'database' => 'drupal_introduction',
+  'username' => 'root',
+  'password' => 'root',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+// Config Sync
+$config_directories['sync'] = 'sites/default/files/config/sync';
+
+// Disable cache
+$settings['container_yamls'][] = __DIR__ . '/services.local.yml';
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+$settings['cache']['bins']['page'] = 'cache.backend.null';
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
